d36e5bd72f1340042c77265e35a9e7b8bd4d0432
alamincoders <developeralamin03@gmail.com>
2023-03-20 20:46:57 +0600

    vite, react and taiwlind starter app

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="h-screen bg-slate-50 flex justify-center items-center">
+      <div className="text-center space-y-3">
+        <h1 className="text-5xl">Vite + React + Tailwind</h1>
+        <p className="text-red-500">Please Check App.jsx and write your code!</p>
+        <p className="text-blue-500">Happy Coding</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
